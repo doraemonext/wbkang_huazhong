7a206615b553e60c51e45ca8db6d508821930750
doraemonext <doraemonext@gmail.com>
2016-08-07 13:57:04 +0800

    add calc 1 to n

--- wechat/urls.py
+++ wechat/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
-from wechat.views import LoginView, MainSelectorView, CalcSelectorView
+from wechat.views import LoginView, MainSelectorView, CalcSelectorView, Calc1ToNView
 
 urlpatterns = [
     url(r'^login$', LoginView.as_view(), name='login'),
     url(r'^main_selector$', MainSelectorView.as_view(), name='main_selector'),
     url(r'^calc_selector$', CalcSelectorView.as_view(), name='calc_selector'),
+    url(r'^calc_1ton$', Calc1ToNView.as_view(), name='calc_1ton'),
 ]
--- wechat/views.py
+++ wechat/views.py
@@ -39,3 +39,16 @@ class CalcSelectorView(View):
         return render(request, 'wechat/calc_selector.html', {
             'openid': request.GET.get('openid', '')
         })
+
+
+class Calc1ToNView(View):
+    """
+    一个R3客户对应多个业务 View
+    """
+    def get(self, request, *args, **kwargs):
+        if not request.session.get('identifier'):
+            return redirect(reverse('wechat:login') + '?openid=' + request.GET.get('openid', ''))
+        return render(request, 'wechat/calc_1ton.html', {
+            'openid': request.GET.get('openid', '')
+        })
+
